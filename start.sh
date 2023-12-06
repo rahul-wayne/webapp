d768efe3322daf7901bdd585a578c82cc34c27ec
blackpearl <58177891+rahul-wayne@users.noreply.github.com>
2023-12-06 21:10:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
-# Start Apache
-rm -rf /run/httpd/httpd.pid
-sleep infinity
+#!/bin/bash
+
+# Remove Apache PID file
+rm -rf /var/run/apache2/apache2.pid
+
+# Start Apache in the foreground
+apache2ctl -D FOREGROUND
