cb39f7ebd044a9989acc06409c5c4e9210133ac5
Rahul Thekkekara <rahulthekkekara@Rahuls-MacBook-Air.local>
2023-12-16 06:25:13 +0530

    pp

--- helm_upgrade.sh
+++ helm_upgrade.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-helm upgrade webapp  https://rahul-wayne.github.io/helm-charts/webapp/charts/webapp-1.2.5.tgz
+helm upgrade webapp  https://rahul-wayne.github.io/helm-charts/webapp/charts/webapp-1.2.6.tgz
